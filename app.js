6e6e71fdc96280a2d2363a7b1a67ea34f5f8a89e
Daniyal-Raza__rizvi <76169564+Daniyal-Raza-450@users.noreply.github.com>
2021-01-06 16:01:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,193 @@
+// chapter no 
+
+// 1
+// alert("Error! Please enter a valid password.")
+
+// 2
+// alert("Welcome to JS Land\nHeppy coding")
+
+// 4
+// alert("Welcome to JS Land...")
+// console = ("")
+// alert(a)
+
+// 5
+// alert("Hello... I can JS through my web browser's console")
+
+// second chapter 
+// VARUABKES FOR STRINGS\
+// 1
+// alert("Jhone Doe")
+// alert("15 years old")
+// alert("Cretified Mobile Application Development")
+
+// 5
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")\
+
+// 6
+
+// alert("My email address is example@example.com")
+
+// 7
+// alert("I am trying to learm from the Book A smarter\nway to learm java Script")
+
+// 9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// 3 chapter
+// VARIABLES FOR NUMBERS
+
+// 1
+// alert("I am 15 years old")
+
+// 2
+// alert("you have visited this side 14 times")
+// var birth=1990;
+// document.write("My birth year is" + birth + '<br>' +" Date type of my ddeclared variable is number")
+
+// 4
+// document.write("<b>" + "john doe" + "</b>" + " ordered" + "<b>" + " 5 T-shirt"+"</b>" + "(s)" + " on XYZ Clothing store" )
+ 
+// chapter 4
+// VARIABLE NAMES: LEGAL & ILLEGAL
+// document.write("<b>" + "Rules for naming JS varlables" + "</b>" + "<br>" + " variable name can only contain . numbers, $ and _ . Fpr example : $my _1stvariable " + "<br>" + " variavle must begin with a letter,$ or _, For example ; $name,_name or name " + "<br>" + " varible names are case sensitive"+"<br>"+"variable names shoukd not be JS keywords")  
+//  chapter 5
+
+// 1
+// var a =3;
+// var b =5;
+// var c =a+b;
+// alert(c)
+
+// 2
+// var a =3;
+// var b =5;
+// var c =a*b;
+// alert(c)
+
+// var a =3;
+// var b =5;
+// var c =a/b;
+// alert(c)
+
+// 5
+// document.write('Table of 4'+ '<br>' + '4*1=4'+ '<br>' + '4*2=8'+ '<br>' + '4*3=12'+ '<br>' + '4*4=16'+ '<br>' + '4*5=20'+ '<br>' + '4*6=24'+ '<br>' + '4*7=28'+ '<br>' + '4*8=32'+ '<br>' + '4*9=36'+ '<br>' + '4*10=40')
+
+// 8
+// document.write('<b>'+'Marks Sheet' +'</b>'  + '<br>'+'Total markes:100'+'<br>'+'Marks obtained:80'+'<br>'+'percentage:80%')
+// var per = prompt('Enter your percentage');
+// if(per <=100 && per >80){
+//     alert('A')
+// }
+// else if (per <=80 ){
+//     alert('none')
+// }
+
+// chapter 6
+// 2
+// var g = 6;
+// var h=--g + ++g -g -- +g;
+// alert(h)
+//  3
+//  var a='daniyal'
+//  alert(a)
+// 4.5
+
+// var a = prompt('enter your namber','Number')
+// var b=+2;
+// var c= a+b;
+// alert(c)
+// 7
+// document.write('<b>'+'Subject Total Marks Obtained Marks Percentage'+'</b>' +'<br>')
+// document.write('English    100      54       54%'+'<br>')
+// document.write('Math    100      54       54%'+'<br>')
+// document.write('Urdu    100      54       54%'+'<br>')
+
+// chapter no 9
+
+// var a=prompt('Enter your city','city Name');
+// var Karachi='Welcome to city of light'
+// var c = a+Karachi;
+// alert(c)
+
+// 2
+
+
+// var gender = 'male';
+// if (gender == 'male'){
+//     document.write('Good morning sir')
+// }else{
+//     document.write('Good morning miss')
+
+// }
+
+// var car =0.25;
+// if(car = 0.25) {
+//     document.write('Please refill the fuel in your car')
+
+// }
+//  5
+
+// var a= 4
+// var b = 3
+// var c =a-- -b++ -b -- -b +b;
+// alert(c)
+
+// chapter 12.13
+// 6
+
+
+// var hour=16;
+// if (hour<15){
+//     greeting ='Good day'
+//     alert(greeting)
+// }else if (hour>15){
+//     greeting='Good evening'
+//     alert(greeting)
+
+// }
+
+// chapter 14 - 16
+
+// 
+// var names=['ali','ahmad','akber','aftab']
+// for (var i=0; i<names.length; i++){
+//     document.write(names[i]+ '<br/>')
+// }
+
+// 
+
+// var names=['1','2','3','4']
+// for (var i=0; i<names.length; i++){
+//     document.write(names[i]+ '<br/>')
+// }
+
+// 7
+
+// document.write('Qualification'+'<br/>' )
+// var names=['1) SSC','2) HSc','3)BCS','4)BS','5)BCOM','6)MS','7)M.Phil.','8)PhD']
+// for (var i=0; i<names.length; i++){
+//     document.write(names[i]+ '<br/>')
+// }
+ 
+
+// chapter 17-20
+
+// 3
+
+// var a = prompt('Welcome to ABC Bakery. What do you want to order sir/ma.am?')
+// var names='cookie';
+// if (names== 'cookie'){
+
+//     document.write('cookie is ','<b>'+'avilable'+'</b>','at index 2 in our bakery')
+// }else{
+//     document.write('not')
+// }
+var gender = "male"
+if( gender ="male"){
+    alert(gender)
+    document.write(alert)
+}else{
+    document.write("none")
+}
